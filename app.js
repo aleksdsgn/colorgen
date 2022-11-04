2ee67b71ac2f068d966cc7a912dbd3ff2c2aa77e
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2022-11-04 20:35:54 +0600

    feat: create block color column

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const cols = document.querySelectorAll('.col');
 
 document.addEventListener('keydown', event => {
+  event.preventDefault();
   if (event.code.toLowerCase() === 'space') {
     setRandomColors();
   };
+})
+
+document.addEventListener('click', (event) => {
+  const type = event.target.dataset.type;
+
+  if (type === 'lock') {
+    console.log('perform lock');
+    console.log(event.target);
+
+    if (type === 'lock') {
+      const node = event.target.tagName.toLowerCase() === 'i'
+        ? event.target
+        : event.target.children[0]
+
+      node.classList.toggle('fa-lock-open');
+      node.classList.toggle('fa-lock');
+    }
+  }
 })
 
 // генерация случайного цвета
@@ -20,10 +39,17 @@ document.addEventListener('keydown', event => {
 // установка случайного цвета в колонку
 function setRandomColors() {
   cols.forEach((col) => {
+    // определяем заблокирована ли колонка
+    const isLocked = col.querySelector('i')
+      .classList.contains('fa-lock');
+
     const text = col.querySelector('h2');
     const button = col.querySelector('button');
-    // const color = generateRandomColor();
     const color = chroma.random();
+
+    if (isLocked) {
+      return;
+    }
 
     text.textContent = color;
     col.style.background = color;
